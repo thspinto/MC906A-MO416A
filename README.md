aea138921449d69203ae2c9b86b11b73ca20362e
Thiago Pinto <thspinto@gmail.com>
2016-04-23 22:29:24 -0300

    Created parsers for data files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Optimized selection o SCRUM stories for sprints
+
+Using genetic algorithm to optimize value of implemented stories.
+
+## Execution environment
+
+This project uses Python3.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # Copyright 2016 (C) Thiago Pinto.
 #
@@ -11,6 +11,10 @@
     teams otimizing the project's cost and aggregated value.
 """
 __author__ = "Thiago Pinto"
+
+import sys
+import csv
+import json
 
 
 def main():
@@ -26,6 +30,48 @@ def main():
         Output:
 
     """
+
+    check_args()
+    backlog = parse_csv(sys.argv[1])
+    team_specs = parse_csv(sys.argv[2])
+    parameters = parse_json(sys.argv[3])
+
+    for config in parameters:
+        #execute algorithm with configuration
+
+
+
+def parse_json(file_name):
+    """Parse JSON file
+    """
+    with open(file_name) as jfile:
+        return json.loads(jfile.read())
+
+
+def parse_csv(file_name):
+    """Parse CSV file
+
+        Parses de CSV file and returns the data without the header.
+    """
+    data = []
+    with open(file_name, encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            data.append(row)
+    return data[1:]
+
+
+def check_args():
+    """Check command line arguments and print help text
+
+        Expected call:
+            ./main.py <backlog.csv> <teams.csv> <alg_config.json>
+
+        File format example can be seen in /data directory
+    """
+    if len(sys.argv) != 4:
+        print("Expected arguments: ./main.py <backlog.csv> <teams.csv> <alg_config.json>")
+        exit(-1)
 
 
 if __name__ == '__main__':
