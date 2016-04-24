0d22a64399710de6cc58ae42ca088c74e256e471
Thiago Pinto <thspinto@gmail.com>
2016-04-23 21:00:16 -0300

    Project setup

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#
+# Copyright 2016 (C) Thiago Pinto.
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+"""Scrum story attribution algorithm
+
+        Genetic algorithm that attributes stories to the next sprint of the development
+    teams otimizing the project's cost and aggregated value.
+"""
+__author__ = "Thiago Pinto"
+
+
+def main():
+    """Import information and execute algorithm
+
+        Import scrum data, algorithm configuration and execute algorithm.
+
+        Input:
+            backlog: backlog in CSV
+            team_specs: team specifications in CSV
+            parameters: algorithm configuration in alg_config.json
+
+        Output:
+
+    """
+
+
+if __name__ == '__main__':
+    sys.exit(main())
